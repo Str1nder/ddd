41d558f1c22915516310abcd70034aaf16537910
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-21 20:54:26 -0300

    Aula 21/03

--- 03-Metodos/src/br/com/fiap/motors/model/Carro.java
+++ 03-Metodos/src/br/com/fiap/motors/model/Carro.java
@@ -11,11 +11,36 @@ public class Carro {
 	
 	//Metodos
 	//idade calcularIdade(ano atual)
-	public int calcularIdade(int anoAtual) {
+	int calcularIdade(int anoAtual) {
 		//Calcular a idade
 		int idade = anoAtual - ano;
 		//Retornar a idade
 		return idade;
+
+}
+	
+	
+// Aumantar o valor do carro 
+	void atualizarValor(double porcentagem) {
+		valor *= (1 + porcentagem);
 	}
+ // Criar um método que calcula e retorna o valor do seguro do carro
+	// Seguro do carro e baseado no valor, 5% do valor do carro
+	
+	double calcularSeguro() {
+		double seguro = valor * 0.05;
+		return seguro;
+	}
+		
+	//Criar um metodo que recebe a quantidade de parcelas, calcula e retorna
+	//o valor da parcela do seguro
+		
+ public double valorParcela(int quantParcela, double seguro) {
+	double valorParcela = seguro / quantParcela;
+	return valorParcela;
+	
+	}
+
 	
 }
+	
--- 03-Metodos/src/br/com/fiap/motors/model/View.java
+++ 03-Metodos/src/br/com/fiap/motors/model/View.java
@@ -17,5 +17,24 @@ public class View {
 		
 		//Exibir a idade
 		System.out.println("Idade do carro: " + i);
+		
+		
+		// Aumentar o valor do carro em 20%
+		gol.atualizarValor(0.2);
+		
+		//Exibir o valor do carro
+		System.out.println("Valor do carro: " + gol.valor);
+		
+		//Calcular e exibir o valor do seguro
+		double seguro = gol.calcularSeguro();
+		System.out.println("Valor do seguro: " + seguro);
+		
+		//calcular e exibir o valor da parcela do seguro
+		double valorParcela = gol.valorParcela(3, seguro);
+		System.out.println("Valor da parcela(3): " + valorParcela);
+		
+	
+	
+		
 	}
 }
